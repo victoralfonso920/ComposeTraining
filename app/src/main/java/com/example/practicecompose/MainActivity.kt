40bf65a9416c24791a2f4cb58745105605c7ae5f
Victor Hernandez <victor.hernandez@hugoapp.com>
2021-05-01 20:11:10 -0600

    add function TextStyling to try Style and options in text witch Compose

--- app/src/main/java/com/example/practicecompose/MainActivity.kt
+++ app/src/main/java/com/example/practicecompose/MainActivity.kt
@@ -22,7 +22,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,62 +39,108 @@ import com.example.practicecompose.ui.theme.PracticeComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.gotan_ultra_bold)
+        )
         setContent {
-            //Greeting()
-            val painter = painterResource(id = R.drawable.rana)
-            val description = "Kermit playing in the snow"
-            val title = "Kermit title in the snow"
-            Box(modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(16.dp)
-            ){
-                ImageCard(painter = painter, contentDescription = description, title = title)
-            }
+            // ShowImage()
+            TextStyling(fontFamily = fontFamily)
 
         }
     }
 }
 
 @Composable
-fun TextStyling() {
+fun TextStyling(fontFamily: FontFamily) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF101010))
+    ) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize = 50.sp
+                    )
+                ) {
+                    append("J")
+                }
+                append("etpack")
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize = 50.sp
+                    )
+                ) {
+                    append("C")
+                }
+                append("ompose")
 
+            },
+            color = Color.White,
+            fontSize = 30.sp,
+            fontFamily = fontFamily,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold,
+            textDecoration = TextDecoration.Underline
+        )
+    }
 }
+
+@Composable
+fun ShowImage() {
+    val painter = painterResource(id = R.drawable.rana)
+    val description = "Kermit playing in the snow"
+    val title = "Kermit title in the snow"
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(16.dp)
+    ) {
+        ImageCard(painter = painter, contentDescription = description, title = title)
+    }
+}
+
 @Composable
 fun ImageCard(
     painter: Painter,
     contentDescription: String,
     title: String,
     modifier: Modifier = Modifier
-){
+) {
     Card(
         modifier = modifier.fillMaxWidth(),
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
     ) {
-        Box(modifier = Modifier.height(200.dp)){
+        Box(modifier = Modifier.height(200.dp)) {
             Image(
                 painter = painter,
                 contentDescription = contentDescription,
                 contentScale = ContentScale.Crop
             )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ),
-                        startY = 300f
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
                     )
-                )
             )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp),
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
                 contentAlignment = Alignment.BottomStart
-            ){
-                Text(text = title,style = TextStyle(color = Color.White,fontSize = 16.sp) )
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
             }
         }
     }
@@ -96,6 +150,6 @@ fun ImageCard(
 @Composable
 fun DefaultPreview() {
     PracticeComposeTheme {
-        Greeting()
+        ShowImage()
     }
 }
