58c8933b92244d28c676f0fea6bef55f386a162a
Victor Hernandez <victor.hernandez@hugoapp.com>
2021-05-02 15:38:57 -0600

    add prectice color state from views in evento Click

--- app/src/main/java/com/example/practicecompose/MainActivity.kt
+++ app/src/main/java/com/example/practicecompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.practicecompose
 
+import android.annotation.SuppressLint
 import android.icu.text.CaseMap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,6 +8,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -14,6 +16,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
@@ -35,6 +39,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.practicecompose.ui.theme.PracticeComposeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,10 +49,45 @@ class MainActivity : ComponentActivity() {
         )
         setContent {
             // ShowImage()
-            TextStyling(fontFamily = fontFamily)
+            // TextStyling(fontFamily = fontFamily)
+            Column(Modifier.fillMaxSize()) {
+                val color =  remember {
+                    mutableStateOf(Color.Yellow)
+                }
+
+                ColorBox(Modifier.weight(1f).fillMaxSize()){
+                    color.value = it
+                }
+                Box(modifier = Modifier
+                    .background(color.value)
+                    .weight(1f)
+                    .fillMaxSize()
+                )
+            }
+
 
         }
     }
+}
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: (Color) -> Unit
+){
+    Box(modifier = modifier
+        .background(color = Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f
+            )
+            )
+        }
+    )
 }
 
 @Composable
